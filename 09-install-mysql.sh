8eb1643bd9d1e365f87ed2718970538f9219ffa5
ramakrishna <kunchalaramakrishna9@gmail.com>
2024-01-20 22:16:36 +0530

    add 9th install mysql executing script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,8 +4,26 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR : please run this script with root access"
+    exit 1
 else
     echo "you are root user"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR :: Installing mysql is failed"
+    exit 1
+elase
+    echo "installing mysql success"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR :: Installing Git is failed"
+elase
+    echo "installing Git success"
+fi
