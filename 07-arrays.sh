6cb867a9ca1dbaa62a98e8d60b1533c51ac26226
ramakrishna <kunchalaramakrishna9@gmail.com>
2024-01-20 21:31:46 +0530

    add 7th arrylist executing script

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "Frist  value :: ${FRUITS[0]}"
+echo "Second value :: ${FRUITS[1]}"
+echo "Thrid value  :: ${FRUITS[2]}"
+
+echo "All Fruits :: ${FRUITS[@]}"
