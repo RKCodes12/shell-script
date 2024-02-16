f2a736cfb04b6c2e68f1e8bb3c9a1241de46093d
ramakrishna <kunchalaramakrishna9@gmail.com>
2024-02-16 20:14:17 +0530

    shell- script

--- mail.sh
+++ mail.sh
@@ -6,8 +6,9 @@ BODY=$3
 ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
+TEXT_FILE=$(uuencode /home/centos/shell-script/text.txt);
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "uuencode /home/centos/shell-script/text.txt" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html $TEXT_FILE")" "$TO_ADDRESS"
 
